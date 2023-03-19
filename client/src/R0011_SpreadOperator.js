990214085ded4fb9a3048c9b547891710770d74a
solarthenomad <oakikim@ewhain.net>
2023-03-19 10:46:27 +0900

    내가 만든 cookie~

--- client/src/R0011_SpreadOperator.js
+++ client/src/R0011_SpreadOperator.js
@@ -12,6 +12,33 @@ class R0011_SpreadOperator extends Component {
         //javascript Array 생성해보기
         var varArray1 = ['num1', 'num2'];
         var varArray2 = ['num3', 'num4'];
-        var sumVarArr = [varArray1[0], varArray1[1], varArray2[0], ]
+        var sumVarArr = [varArray1[0], varArray1[1], varArray2[0], varArray2[1]];
+        //var sumVarArr = [].concat(varArray1, varArray2); 와 같다.
+        console.log('1.sumVarArr : ' + sumVarArr);
+
+        //ES6식 Array 
+        let sumLetArr = [...varArray1, ...varArray2];
+        console.log('2.sumLetArr:' + sumLetArr );
+        const [sum1, sum2, ...remain] = sumLetArr;
+        console.log('3.sum1 :' + sum1 +',sum2 : '+sum2+',remain :' + remain);
+
+        var varObj1 = {key1 : 'val1', key2 : 'val2'};
+        var varObj2 = {kry2 : 'new2', key3 : 'val3'};
+        //javascript Object
+        var sumVarObj = Object.assign({},varObj1, varObj2);
+        console.log('4.sumVarObj:' + JSON.stringify(sumVarObj));
+        //ES6 Object방식으로 표현하기
+        var sumLetObj = {...varObj1, ...varObj2};
+        console.log('5.sumLetObj :' + JSON.stringify(sumLetObj));
+        var {key1 , key3, ...others} =sumLetObj;
+        console.log('6.key1 : ' + key1+ ',key3 :' +key3 + ',others :' +JSON.stringify(others));
+
+    }
+    render() {
+        return(
+            <h2>[THIS IS SpreadOprator]</h2>
+        )
     }
 }
+
+export default R0011_SpreadOperator;

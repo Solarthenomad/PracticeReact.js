09c15dd4b2db8230aca7561f8ead917fb7261cf9
solarthenomad <oakikim@ewhain.net>
2023-03-20 11:14:01 +0900

    3월 20일 커밋1

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Es6 from './R009_Es6';
 import SpreadOprator from './R0011_SpreadOperator';
 import Jquery from './R016_Jquery';
 import Props from './R017_Props';
+import PropsDatatype from './R018_PropsDatatype';
+import PropsBoolean from './R019_PropsBoolean';
 
 
 //App.js와 같은 폴더 경로에 위치한 App.css 임포트하기 방법 
@@ -25,6 +27,22 @@ function App() {
       <SpreadOprator/>
       <Jquery/>
       <Props props_val = "This is Props"/>
+      <PropsDatatype
+         String = "react"
+         Number = {200}
+         Boolean = {1==1}
+         Array = {[0,1,0]}
+         ObjectJson = {{react:"리액트", twohundred:"200"}}
+         Function = {console.log("FunctionProps : function!")}
+         //문자열(String), 숫자(Number), 불리언, 배열, 객체(ObjectJson)
+         //자식 컴포넌트에서 props에 대한 자료형을 선언해 놓으면 부모 컴포넌트에서 넘어오는 props 변수들의 자료형과 비교함
+         //이때자료형이 일치하지 않는다면 결고 메세지로 알려주기 때문에 잘못된 데이터를확인하는 것이 가능하다. 
+      />
+      <PropsBoolean BooleanTrueFalse= {false}/>
+      //Booleanprops 변수를 선언해주고 false값을 담아 props에 전달해준다. 
+      <PropsBoolean BooleanTrueFalse/>
+
+      
       
     </div>
     //App.js에서 임포트한 component인 R006_LifecycleEx로 prop_value라는 변수를 전달한다.
--- client/src/R018_PropsDatatype.js
+++ client/src/R018_PropsDatatype.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { Component} from "react";
 import datatype from 'prop-types';
-
+//자식 컴포넌트에서 props에 대한 자료형을 
 class R018_PropsDatatype extends Component {
     render () {
         let {
-            String, Number, Boolean, Array, Object Json, Function
+            //지역변수를 선언하여 props로 전달된 값들을 (부모 컴포넌트에서 전달받은 값들)을 할당해준다. 
+            String, Number, Boolean, Array, ObjectJson, Function
         } = this.props
         return (
             <div style={{padding:"0px"}}>
                 <p> StringProps : {String}</p>
+                <p>NumberRos : {Number}</p>
+                <span>Booleanprops : {Boolean.toString()}</span>
+                <p>ArrayProps:{Array.toString()}</p>
+                <p>Object JsonProps : {Json.stringfy(ObjectJson)}</p>
+                <p>Function Props : {Function}</p>
+                //여기서 App.js에서 넘어온값들이 데이터타입과 맞는지를 검사하고 맞다면 그 값들을 출력해준다. 
+
                 
             </div>
         )
     }
 }
+//이 컴퓨넌트에서 props에 대한 자료형들을 선언해놓았음 
+R018_PropsDatatype.propTypes = {
+    String : datatype.number,
+    Number:datatype.number,
+    Boolean : datatype.bool,
+    Array : datatype.array,
+    ObjectJson : datatype.object,
+    Function : datatype.func,
+
+}
+
+export default R018_PropsDatatype;

78d0f04fb0f8baa3529ce1f6f32c6e1131857021
solarthenomad <oakikim@ewhain.net>
2023-04-02 01:54:40 +0900

    나한ㅌ 왜그래 대체 왜그래... 나도 사람이야ㅠㅠㅠㅠㅠㅠ그만할래

--- client/src/R048_ReactstrapModal.js
+++ client/src/R048_ReactstrapModal.js
@@ -18,7 +18,22 @@ class R048_ReactstrapModal extends Component {
    render() {
     return (
         <>
-        <Button color = "warning" onClick={this.toggle}></Button>
+        <Button color = "warning" onClick={this.toggle}>Modal 버튼</Button>
+        <Modal>
+            <ModalHeader toggle={this.state.modal}>Moddal Header
+
+            </ModalHeader>
+            <ModalBody>
+                The generated Lorem Ipsum is therefore always free from repetition.
+
+            </ModalBody>
+            <ModalFooter>
+                <Button color="primary" onClick={this.toggle}>확인</Button>
+
+                <Button color="secondary" onClick={this.toggle}>닫기</Button>
+
+            </ModalFooter>
+        </Modal>
         </>
     )
    }
---
+++ client/src/R049_ReactstrapNavbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Component} from 'react';
+import {Navbar, NavbarToggler,NavbarBrand, Nav, NavItem, NavLink, Collapse  } from 'reactstrap';
+
+class R049_ReactstrapNavbar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {collapsed:false}
+    }
+
+    toggle=() =>{
+        
+    }
+}

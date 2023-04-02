987c583140723fdf91f06ca271d48e7e729f22a8
solarthenomad <oakikim@ewhain.net>
2023-04-02 15:58:25 +0900

    커밋

--- client/src/R049_ReactstrapNavbar.js
+++ client/src/R049_ReactstrapNavbar.js
@@ -9,6 +9,33 @@ class R049_ReactstrapNavbar extends Component {
     }
 
     toggle=() =>{
-        
+        this.setState = ({collapsed:!this.state.collapsed})
     }
+
+    render() {
+        return (
+            <>
+            <Navbar color="faded" light>
+                <NavbarBrand href="/" className='mr-auto'>Navbar</NavbarBrand>
+                <NavbarToggler onClick={this.toggle} className="mr-2"/>
+                <Collapse isOpen={this.state.collapsed} navbar>
+                   <Nav navbar>
+                    <NavItem>
+                        <NavLink href="#"></NavLink>
+
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="http://example.com/">200</NavLink>
+                    </NavItem>
+                   </Nav>
+                
+                </Collapse>
+            </Navbar>
+            </>
+        )
+    }
+
+
 }
+
+export default R049_ReactstrapNavbar;

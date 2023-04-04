ebbfa144b8336a59b9c51ada760349869ad03df0
solarthenomad <oakikim@ewhain.net>
2023-04-04 22:36:03 +0900

    오늘 새벽 4시까지 빡공 ㄱㄱ

--- client/src/R059_FetchGet.js
+++ client/src/R059_FetchGet.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 
 class R059_FetchGet extends Component {
+    componentDidMount= async ()=>{
+        const response = await fetch('http://date.jsontest.com/');
+        const body = await response.json();
+        alert(body.date);
+    }
     
     render() {
         return (
@@ -8,3 +13,5 @@ class R059_FetchGet extends Component {
         )
     }
 }
+
+export default R059_FetchGet;

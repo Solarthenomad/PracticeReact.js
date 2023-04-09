c1e91421f47e6fccd8bbb952b179aed667ce419d
solarthenomad <oakikim@ewhain.net>
2023-04-09 22:03:07 +0900

    children

--- client/src/Context/R076_ContextApi.js
+++ client/src/Context/R076_ContextApi.js
@@ -5,5 +5,14 @@ const {Provider, Consumer} = React.createContext()
 export {Consumer}
 
 class R076_ContextApi extends React.Component {
-    
+    render() {
+    return (
+        <Provider value = "React200">
+            <Children />
+
+        </Provider>
+    )
+    }
 }
+
+export default R076_ContextApi;
